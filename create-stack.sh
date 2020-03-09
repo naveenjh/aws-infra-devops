c3f5322afccd1e608eba57ec1f757b176b08bdd4
PTCNET\njha <njha@ptc.com>
2020-03-09 14:45:46 -0700

    update website

--- create-stack.sh
+++ create-stack.sh
@@ -5,4 +5,5 @@ aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
---region=us-west-2
+--region=us-east-1 \
+--capabilities CAPABILITY_NAMED_IAM
